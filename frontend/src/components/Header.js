c72e3bc4e75d67766edf6ca54c2df1bbc3d7d402
Teemu Holappa <teemu.holappa@boogiesoftware.com>
2022-11-06 15:59:32 +0200

    Commit last build to get the latest changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
         <h1 className={classes.logo}>NHLbets</h1>
         <nav className={classes.navLinks}>
-            <NavLink to="/ongoing">Ongoings</NavLink>
+            <NavLink to="/ongoing">Ongoing</NavLink>
             <NavLink to="/upcoming">Upcoming</NavLink>
             <NavLink to="/results">Results</NavLink>
             <NavLink to="/leaderboard">Leaderboard</NavLink>
